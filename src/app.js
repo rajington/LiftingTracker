372e6e4fd9e069761748fa0e76b5f20464cf911d
Rajarshi Nigam <rajington@users.noreply.github.com>
2015-02-10 14:23:54 -0500

    basic displaying of fake data

--- src/app.js
+++ src/app.js
@@ -5,54 +5,79 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+// fake out configuration information for now
+var exercises = [];
+for (var i=0; i < 10; i++) {
+  var name = 'Exercise '+i;
+  var starting = 100;
+  exercises.push({
+    name: name,
+    reps: [starting, starting+5, starting+10]
+  });
+}
+
+// menu item for each exercise
+var items = exercises.map(function(exercise){
+  return {
+    title: exercise.name,
+    subtitle: exercise.reps.join()
+  };
+});
+
+var main = new UI.Menu({
+  sections: [{
+    items: items        
+  }]
+});
+
+main.on('select', function(e) {
+  var exercise = exercises[e.itemIndex];
+  var card = new UI.Card();
+  card.title(exercise.name);
+  card.subtitle(exercise.reps.join());
+  card.show();
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+// main.on('click', 'up', function(e) {
+//   var menu = new UI.Menu({
+//     sections: [{
+//       items: [{
+//         title: 'Pebble.js',
+//         icon: 'images/menu_icon.png',
+//         subtitle: 'Can do Menus'
+//       }, {
+//         title: 'Second Item',
+//         subtitle: 'Subtitle Text'
+//       }]
+//     }]
+//   });
+//   menu.on('select', function(e) {
+//     console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
+//     console.log('The item is titled "' + e.item.title + '"');
+//   });
+//   menu.show();
+// });
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
+// main.on('click', 'select', function(e) {
+//   var wind = new UI.Window();
+//   var textfield = new UI.Text({
+//     position: new Vector2(0, 50),
+//     size: new Vector2(144, 30),
+//     font: 'gothic-24-bold',
+//     text: 'Text Anywhere!',
+//     textAlign: 'center'
+//   });
+//   wind.add(textfield);
+//   wind.show();
+// });
 
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
-});
+// main.on('click', 'down', function(e) {
+//   var card = new UI.Card();
+//   card.title('A Card');
+//   card.subtitle('Is a Window');
+//   card.body('The simplest window type in Pebble.js.');
+//   card.show();
+// });
